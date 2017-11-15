f6a6ca79f89cb7a1d0248966b7694f64e8d67951
Julien Schueller <schueller@phimeca.com>
2017-11-15 22:46:50 +0100

    Add upload-pypi.sh

---
+++ upload-pypi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VERSION=1.10
+
+set -e -x
+for ABI in cp27m cp27mu cp35m cp36m
+do
+  PYVER=`echo ${ABI}| cut -c3-4`
+  wget -c https://github.com/openturns/build-wheel/releases/download/v${VERSION}/openturns-${VERSION}-cp${PYVER}-${ABI}-manylinux1_x86_64.whl -P /tmp
+  twine upload -r test /tmp/openturns-${VERSION}-cp${PYVER}-${ABI}-manylinux1_x86_64.whl
+done
