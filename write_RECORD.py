0d79e1d0b418b1b13214c08d051b5509d61ea767
Julien Schueller <schueller@phimeca.com>
2018-04-11 14:12:45 +0200

    Cleanup wheel

--- write_RECORD.py
+++ write_RECORD.py
@@ -12,12 +12,16 @@ version = sys.argv[1]
 path = os.path.join('openturns-' + version + '.dist-info', 'RECORD')
 
 with open(path, 'w') as record:
-    for f in os.listdir('openturns'):
-        fpath = os.path.join('openturns', f)
-        size = os.path.getsize(fpath)
-        if os.path.isfile(fpath):
-            data = open(fpath, 'rb').read()
-            digest = hashlib.sha256(data).digest()
-            checksum = base64.urlsafe_b64encode(digest).decode()
-            size = len(data)
-            record.write(fpath + ',sha256=' + checksum + ',' + str(size) + '\n')
+    for subdir in ['openturns', 'openturns-' + version + '.dist-info']:
+        for f in os.listdir(subdir):
+            fpath = os.path.join(subdir, f)
+            size = os.path.getsize(fpath)
+            if os.path.isfile(fpath):
+                data = open(fpath, 'rb').read()
+                digest = hashlib.sha256(data).digest()
+                checksum = base64.urlsafe_b64encode(digest).decode()
+                size = len(data)
+                if not 'RECORD' in fpath:
+                    record.write(fpath + ',sha256=' + checksum + ',' + str(size) + '\n')
+                else:
+                    record.write(fpath + ',,\n')
