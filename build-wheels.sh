ed4565920998b6e41c67830f620ffb9d45da105b
Julien Schueller <schueller@phimeca.com>
2018-04-11 17:41:17 +0200

    Try copy conf file

--- build-wheels.sh
+++ build-wheels.sh
@@ -28,14 +28,15 @@ cmake -DCMAKE_INSTALL_PREFIX=$PWD/install -DUSE_SPHINX=OFF -DLINK_PYTHON_LIBRARY
       -DPYTHON_EXECUTABLE=/opt/python/cp${PYVER}-${ABI}/bin/python ..
 make install -j2
 
-# move conf file next to lib so it can be found using dladr when relocated
-cp $PWD/install/etc/openturns/openturns.conf $PWD/install/lib
-
 # run a few tests
 ctest -R "NLopt|Study|SymbolicFunction|SquareMatrix" -E cppcheck -j2
 
 cd install/lib/python*/site-packages/
 rm -rf openturns/__pycache__ openturns/*.pyc
+
+# move conf file next to lib so it can be found using dladr when relocated
+mkdir -p openturns/.libs
+cp ../../../etc/openturns/openturns.conf openturns/.libs
 
 # write metadata
 mkdir openturns-${VERSION}.dist-info
--- upload-pypi.sh
+++ upload-pypi.sh
@@ -9,3 +9,10 @@ do
   wget -c https://github.com/openturns/build-wheel/releases/download/v${VERSION}/openturns-${VERSION}-cp${PYVER}-${ABI}-manylinux1_x86_64.whl -P /tmp
   twine upload -r test /tmp/openturns-${VERSION}-cp${PYVER}-${ABI}-manylinux1_x86_64.whl
 done
+
+for ABI in cp27m cp36m
+do
+  PYVER=`echo ${ABI}| cut -c3-4`
+  wget -c https://github.com/openturns/build-wheel/releases/download/v${VERSION}/openturns-${VERSION}-cp${PYVER}-${ABI}-win_amd64.whl -P /tmp
+  twine upload -r test /tmp/openturns-${VERSION}-cp${PYVER}-${ABI}-win_amd64.whl
+done
