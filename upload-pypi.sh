e31bed17ee7aa19eacd787eab2f4f304d81e4759
Julien Schueller <schueller@phimeca.com>
2019-01-18 19:07:54 +0100

    macosx_10_10_x86_64

--- upload-pypi.sh
+++ upload-pypi.sh
@@ -27,3 +27,10 @@ do
   wget -c https://github.com/openturns/build-wheel/releases/download/v${VERSION}/openturns-${VERSION}-cp${PYVER}-${ABI}-win_amd64.whl -P /tmp
   twine upload -r test /tmp/openturns-${VERSION}-cp${PYVER}-${ABI}-win_amd64.whl
 done
+
+for ABI in cp27m cp36m cp37m
+do
+  PYVER=`echo ${ABI}| cut -c3-4`
+  wget -c https://github.com/openturns/build-wheel/releases/download/v${VERSION}/openturns-${VERSION}-cp${PYVER}-${ABI}-macosx_10_10_x86_64.whl -P /tmp
+  twine upload -r test /tmp/openturns-${VERSION}-cp${PYVER}-${ABI}-macosx_10_10_x86_64.whl
+done
