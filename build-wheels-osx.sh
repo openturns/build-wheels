54a3470837b6d9d6b5912345f87bbfaf8618d113
Julien Schueller <schueller@phimeca.com>
2019-01-18 16:01:59 +0100

    Repackage conda binaries

--- build-wheels-osx.sh
+++ build-wheels-osx.sh
@@ -22,36 +22,9 @@ wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda3-lates
 bash /tmp/Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --add channels conda-forge
-conda install -y python=${PYVERD} cmake swig muparser openblas bison flex nlopt libxml2 tbb-devel delocate
+conda install -y python=${PYVERD} openturns delocate
 
-rm -rf /tmp/openturns
-git clone https://github.com/openturns/openturns.git /tmp/openturns
-cd /tmp/openturns
-git checkout v${VERSION}
-
-# python_executable=`find /usr/local/Cellar/python/${PYVERD:0:3}* -name python`
-
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PWD/install \
-      -DCMAKE_PREFIX_PATH=${HOME}/miniconda \
-      -DCMAKE_INSTALL_RPATH="${HOME}/miniconda/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_MACOSX_RPATH=ON \
-      -DLAPACK_LIBRARIES="${HOME}/miniconda/lib/libopenblas.dylib" \
-      -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j8" \
-      -DSWIG_COMPILE_FLAGS="-O1" \
-      -DUSE_NLOPT=OFF \
-      ..
-make install -j2
-otool -L /tmp/openturns/build/install/lib/python${PYVERD:0:3}/site-packages/openturns/_common.so
-
-# run a few tests
-ctest -R "NLopt|Study|SymbolicFunction|SquareMatrix" -E cppcheck -j2 --output-on-failure
-
-cd install/lib/python*/site-packages/
-rm -rf openturns/__pycache__ openturns/*.pyc
-
-# move conf file next to lib so it can be found using dladr when relocated
-mkdir -p openturns/.libs
-cp ../../../etc/openturns/openturns.conf openturns/.libs
+cd ${HOME}/miniconda/lib/python${PYVERD}/site-packages/
 
 # write metadata
 mkdir openturns-${VERSION}.dist-info
@@ -66,5 +39,7 @@ delocate-listdeps openturns-${VERSION}-${TAG}.whl
 delocate-wheel -w ${TRAVIS_BUILD_DIR}/wheelhouse -v openturns-${VERSION}-${TAG}.whl
 delocate-listdeps --all ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl
 
+conda remove -y openturns
+rm -r openturns-${VERSION}.dist-info
 pip install openturns --no-index -f ${TRAVIS_BUILD_DIR}/wheelhouse
 python -c "import openturns as ot; print(ot.Normal(3).getRealization())"
