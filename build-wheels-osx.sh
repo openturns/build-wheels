60f6c6da0196e556a00f6e4b7db39e50d859f8d9
Julien Schueller <schueller@phimeca.com>
2019-01-18 21:11:50 +0100

    For loop

--- build-wheels-osx.sh
+++ build-wheels-osx.sh
@@ -38,15 +38,11 @@ cp ../../../etc/openturns/openturns.conf openturns/.dylibs
 zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/openturns.conf
 
 # missing libs
-cp ${HOME}/miniconda/lib/libquadmath.0.dylib openturns/.dylibs
-cp ${HOME}/miniconda/lib/libgcc_s.1.dylib openturns/.dylibs
-cp ${HOME}/miniconda/lib/libc++abi.1.dylib openturns/.dylibs
-cp ${HOME}/miniconda/lib/libgfortran.3.dylib openturns/.dylibs
-zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/libquadmath.0.dylib
-zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/libgcc_s.1.dylib
-zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/libc++abi.1.dylib
-zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/libgfortran.3.dylib
-
+for libname in libquadmath.0 libgcc_s.1 libc++abi.1 libgfortran.3
+do
+  cp ${HOME}/miniconda/lib/${libname}.dylib openturns/.dylibs
+  zip -u ${TRAVIS_BUILD_DIR}/wheelhouse/openturns-${VERSION}-${TAG}.whl openturns/.dylibs/${libname}.dylib
+done
 
 cd ${TRAVIS_BUILD_DIR}
 rm -r ${HOME}/miniconda
