5fef49039120516aeac9aa9eee4581dd562bccb0
Julien Schueller <schueller@phimeca.com>
2024-11-30 10:58:13 +0100

    SWIG_HEAPTYPES

--- build-wheels-linux.sh
+++ build-wheels-linux.sh
@@ -28,7 +28,7 @@ VERSION=`cat VERSION`
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/build/install \
       -DPython_EXECUTABLE=/opt/python/${PYTAG}-${ABI}/bin/python \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
-      -DSWIG_COMPILE_FLAGS="-O1 -DPy_LIMITED_API=0x03090000" \
+      -DSWIG_COMPILE_FLAGS="-O1 -DPy_LIMITED_API=0x03090000 -DSWIG_HEAPTYPES" \
       -B build .
 cd build
 make install
